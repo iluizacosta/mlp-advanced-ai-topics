cb98a6980da53d8953d0ac007759bb9e0956b447
Luiza Costa <analuizacosta7084@gmail.com>
2026-04-30 13:54:03 -0300

    Feat: splitting Train/Test data and KS Test

---
+++ src/split_data.py
@@ -0,0 +1,47 @@
+import pandas as pd
+from scipy.stats import ks_2samp
+from sklearn.model_selection import train_test_split
+
+
+def stratified_split(df: pd.DataFrame, target_col: str, test_size=0.2, random_state=42):
+    """
+    Performs stratified split training and testing.
+    """
+
+    X = df.drop(columns=[target_col])
+    y = df[target_col]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        y,
+        test_size=test_size,
+        stratify=y,
+        random_state=random_state
+    )
+
+    train_df = pd.concat([X_train, y_train], axis=1)
+    test_df = pd.concat([X_test, y_test], axis=1)
+
+    return train_df, test_df
+
+
+def ks_test(train_df: pd.DataFrame, test_df: pd.DataFrame, columns: list) -> dict:
+    """
+    Compares distributions using KS Test only (Kolmogorov-Smirnov).
+    """
+
+    results = {}
+
+    for col in columns:
+        train_vals = train_df[col].dropna()
+        test_vals = test_df[col].dropna()
+
+        stat, p = ks_2samp(train_vals, test_vals)
+
+        results[col] = {
+            "test": "KS",
+            "statistic": stat,
+            "p_value": p
+        }
+
+    return results
